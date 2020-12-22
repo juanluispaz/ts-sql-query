f74e01100be7d7d00ad5d76615fdfd0c210b341a
juanluispaz <juanluispaz@gmail.com>
2020-12-22 21:19:54 +0100

    Braking change, the return of the type adapters (including the default implementation in the connection) have now type unknown

--- src/TypeAdapter.ts
+++ src/TypeAdapter.ts
@@ -1,9 +1,9 @@
 export interface DefaultTypeAdapter {
-    transformValueFromDB(value: unknown, type: string): any
-    transformValueToDB(value: unknown, type: string): any
+    transformValueFromDB(value: unknown, type: string): unknown
+    transformValueToDB(value: unknown, type: string): unknown
 }
 
 export interface TypeAdapter {
-    transformValueFromDB(value: unknown, type: string, next: DefaultTypeAdapter): any
-    transformValueToDB(value: unknown, type: string, next: DefaultTypeAdapter): any
+    transformValueFromDB(value: unknown, type: string, next: DefaultTypeAdapter): unknown
+    transformValueToDB(value: unknown, type: string, next: DefaultTypeAdapter): unknown
 }

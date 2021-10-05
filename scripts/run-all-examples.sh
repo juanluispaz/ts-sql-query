449f2111f674384af9482c57e95c4b098764182e
juanluispaz <juanluispaz@gmail.com>
2021-10-05 02:03:52 +0200

    Add a running mocked version of the examples in the documentation per each supported database

--- scripts/run-all-examples.sh
+++ scripts/run-all-examples.sh
@@ -12,6 +12,14 @@ rm -Rf node_modules/loopback-connector-oracle/node_modules/oracledb
 
 set -x #echo on
 
+ts-node ./src/examples/documentation/Sqlite-compatibility.ts || exit 1
+ts-node ./src/examples/documentation/Sqlite-modern.ts || exit 1
+ts-node ./src/examples/documentation/PostgreSql.ts || exit 1
+ts-node ./src/examples/documentation/MySql.ts || exit 1
+ts-node ./src/examples/documentation/MariaDB.ts || exit 1
+ts-node ./src/examples/documentation/SqlServer.ts || exit 1
+ts-node ./src/examples/documentation/Oracle.ts || exit 1
+
 ts-node ./src/examples/SqliteExample.ts || exit 1
 ts-node ./src/examples/Sqlite3Example.ts || exit 1
 ts-node ./src/examples/BetterSqlite3Example.ts || exit 1
--- src/examples/assertEquals.ts
+++ src/examples/assertEquals.ts
@@ -1,46 +1,73 @@
-export function assertEquals(v1: any, v2: any) {
-    if (Array.isArray(v1)) {
-        if (!Array.isArray(v2)) {
-            throw new Error('not equals (not same type array) ' + JSON.stringify(v1) + ' ' + JSON.stringify(v2))
-        }
-        if (v1.length !== v2.length) {
-            throw new Error('not equals (not same length) ' + JSON.stringify(v1) + ' ' + JSON.stringify(v2))
-        }
-        v1.forEach((_v, index) => {
-            assertEquals(v1[index], v2[index])
-        })
-    } else if (v1 instanceof Date) {
-        if (!(v2 instanceof Date)) {
-            throw new Error('not equals (not same type date) ' + JSON.stringify(v1) + ' ' + JSON.stringify(v2))
-        }
-        if ((v2 as any).__type__ === 'localDate' || (v1 as any).__type__ === 'localDate') {
-            if (!(v1.getFullYear() === v2.getFullYear() && v1.getMonth() === v2.getMonth() && v1.getDate() === v2.getDate())) {
-                throw new Error('not equals (not same localDate) ' + JSON.stringify(v1) + ' ' + JSON.stringify(v2))
-            }
-        }
-        if ((v2 as any).__type__ === 'localTime' || (v1 as any).__type__ === 'localTime') {
-            if (!(v1.getHours() === v2.getHours() && v1.getMinutes() === v2.getMinutes() && v1.getSeconds() === v2.getSeconds() && v1.getMilliseconds() === v2.getMilliseconds())) {
-                throw new Error('not equals (not same localTime)' + JSON.stringify(v1) + ' ' + JSON.stringify(v2))
-            }
-        }
-        if (v1.getTime() !== v2.getTime()) {
-            throw new Error('not equals ' + JSON.stringify(v1) + ' ' + JSON.stringify(v2))
-        }
-    } else if (v1 === null || v1 === undefined) {
-        if (!(v2 === null || v2 === undefined)) {
-            throw new Error('not equals (null or undefined) ' + JSON.stringify(v1) + ' ' + JSON.stringify(v2))
-        }
-    } else if (typeof v1 === 'object') {
-        if (typeof v2 !== 'object') {
-            throw new Error('not equals (not same tipe object) ' + JSON.stringify(v1) + ' ' + JSON.stringify(v2))
-        }
-        for (let key in v1) {
-            assertEquals(v1[key], v2[key])
-        }
-        for (let key in v2) {
-            assertEquals(v1[key], v2[key])
-        }
-    } else if (v1 !== v2) {
-        throw new Error('not equals ' + JSON.stringify(v1) + ' ' + JSON.stringify(v2))
+export function assertEquals(first: any, second: any) {
+    const result = verifyEquals(first, second, '')
+    if (result) {
+        throw new Error(result)
     }
+}
+
+function verifyEquals(first: any, second: any, path: string = '') : string | undefined {
+    if (Array.isArray(first)) {
+        if (!Array.isArray(second)) {
+            return 'not equals (not same type array)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+        }
+        if (first.length !== second.length) {
+            return 'not equals (not same length)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+        }
+        for (let index = 0, length = first.length; index < length; index++) {
+            const result = verifyEquals(first[index], second[index], path + '[' + index + ']')
+            if (result) {
+                return result
+            }
+        }
+    } else if (first instanceof Date) {
+        if (!(second instanceof Date)) {
+            return 'not equals (not same type date)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+        }
+        if ((second as any).___type___ === 'localDate' || (first as any).___type___ === 'localDate') {
+            if (!(first.getFullYear() === second.getFullYear() && first.getMonth() === second.getMonth() && first.getDate() === second.getDate())) {
+                return 'not equals (not same localDate)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+            }
+        } else if ((second as any).___type___ === 'localTime' || (first as any).___type___ === 'localTime') {
+            if (!(first.getHours() === second.getHours() && first.getMinutes() === second.getMinutes() && first.getSeconds() === second.getSeconds() && first.getMilliseconds() === second.getMilliseconds())) {
+                return 'not equals (not same localTime)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+            }
+        } else if (first.getTime() !== second.getTime()) {
+            return 'not equals (date)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+        }
+    } else if (first === null || first === undefined) {
+        if (!(second === null || second === undefined)) {
+            return 'not equals (null or undefined)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+        }
+    } else if (typeof first === 'object') {
+        if (typeof second !== 'object') {
+            return 'not equals (not same object type)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+        }
+        for (let key in first) {
+            const result = verifyEquals(first[key], second[key], path + '.' + key)
+            if (result) {
+                return result
+            }
+        }
+        for (let key in second) {
+            const result = verifyEquals(first[key], second[key], path + '.' + key)
+            if (result) {
+                return result
+            }
+        }
+    } else if (first !== second) {
+        if (typeof first === 'string') {
+            if (typeof second === 'string') {
+                return 'not equals (===)\nFirst:  ' + first + '\nSecond: ' + second + '\nPath: ' + path
+            } else {
+                return 'not equals (===)\nFirst:  ' + first + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+            }
+        } else {
+            if (typeof second === 'string') {
+                return 'not equals (===)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + second + '\nPath: ' + path
+            } else {
+                return 'not equals (===)\nFirst:  ' + JSON.stringify(first) + '\nSecond: ' + JSON.stringify(second) + '\nPath: ' + path
+            }
+        }
+    }
+    return undefined
 }

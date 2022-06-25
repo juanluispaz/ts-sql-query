983ad6d64128cab1c96b050a8d8f06fef09a21be
lorefnon <lorefnon@gmail.com>
2022-06-25 19:21:53 +0530

    Include timestamps in LoggingQueryRunner callbacks; Make the ConsoleLogQueryRunner more configurable so that it can output results, timestamps and durations as well

--- src/queryRunners/ConsoleLogQueryRunner.ts
+++ src/queryRunners/ConsoleLogQueryRunner.ts
@@ -1,12 +1,30 @@
 import type { QueryRunner } from "./QueryRunner"
-import { LoggingQueryRunner, QueryType } from "./LoggingQueryRunner"
+import { LoggingQueryRunner } from "./LoggingQueryRunner"
 
-export class ConsoleLogQueryRunner<T extends QueryRunner> extends LoggingQueryRunner<T> {
-    constructor(queryRunner: T) {
-        super({onQuery: onQuery}, queryRunner)
-    }
+export interface ConsoleLogQueryRunnerOpts {
+    logResults?: boolean;
+    logTimestamps?: boolean;
+    logDurations?: boolean;
 }
 
-function onQuery(queryType: QueryType, query: string, params: any[]): void {
-    console.log(queryType + ':', query, params)
+export class ConsoleLogQueryRunner<T extends QueryRunner> extends LoggingQueryRunner<T> {
+    constructor(queryRunner: T, opts?: ConsoleLogQueryRunnerOpts) {
+        const logEnd = opts?.logResults || opts?.logDurations || opts?.logTimestamps;
+        super({
+            onQuery: (queryType, query, params, timestamps) => {
+                const ts = opts?.logTimestamps ? `[${timestamps.startedAt}]` : '';
+                console.log(`${ts} ${queryType}`, query, params);
+            },
+            onQueryResult: logEnd ? (queryType, query, params, result, timestamps) => {
+                const ts = opts?.logTimestamps ? `[${timestamps.endedAt}]` : '';
+                const duration = opts?.logDurations ? `[Took ${(timestamps.endedAt - timestamps.startedAt)}ns]` : '';
+                console.log(`${ts}${duration} ${queryType}`, { query, params, result })
+            } : undefined,
+            onQueryError: logEnd ? (queryType, query, params, error, timestamps) => {
+                const ts = opts?.logTimestamps ? `[${timestamps.endedAt}]` : '';
+                const duration = opts?.logDurations ? `[Took ${(timestamps.endedAt - timestamps.startedAt)}ns]` : '';
+                console.log(`${ts}${duration} ${queryType}`, { query, params, error })
+            } : undefined
+        }, queryRunner)
+    }
 }

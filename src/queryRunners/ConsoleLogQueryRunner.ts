5618203302c774a1b1503f362f54198f16e85554
lorefnon <lorefnon@gmail.com>
2022-06-26 00:31:43 +0530

    Add support to specify granularity of timestamps/durations

--- src/queryRunners/ConsoleLogQueryRunner.ts
+++ src/queryRunners/ConsoleLogQueryRunner.ts
@@ -1,7 +1,10 @@
 import type { QueryRunner } from "./QueryRunner"
 import { LoggingQueryRunner } from "./LoggingQueryRunner"
 
+type TimeGranularity = 'ms' | 'us' | 'ns'
+
 export interface ConsoleLogQueryRunnerOpts {
+    timeGranularity?: TimeGranularity,
     logResults?: boolean;
     logTimestamps?: boolean;
     logDurations?: boolean;
@@ -10,21 +13,44 @@ export interface ConsoleLogQueryRunnerOpts {
 export class ConsoleLogQueryRunner<T extends QueryRunner> extends LoggingQueryRunner<T> {
     constructor(queryRunner: T, opts?: ConsoleLogQueryRunnerOpts) {
         const logEnd = opts?.logResults || opts?.logDurations || opts?.logTimestamps;
+        const timeGranularity = opts?.timeGranularity ?? 'ms';
         super({
             onQuery: (queryType, query, params, timestamps) => {
-                const ts = opts?.logTimestamps ? `[${timestamps.startedAt}]` : '';
+                const ts = opts?.logTimestamps
+                    ? `[${formatDuration(timestamps.startedAt, timeGranularity)}]`
+                    : '';
                 console.log(`${ts} ${queryType}`, query, params);
             },
             onQueryResult: logEnd ? (queryType, query, params, result, timestamps) => {
-                const ts = opts?.logTimestamps ? `[${timestamps.endedAt}]` : '';
-                const duration = opts?.logDurations ? `[Took ${(timestamps.endedAt - timestamps.startedAt)}ns]` : '';
-                console.log(`${ts}${duration} ${queryType}`, { query, params, result })
+                const ts = opts?.logTimestamps
+                    ? `[${formatDuration(timestamps.endedAt, timeGranularity)}]`
+                    : '';
+                const duration = opts?.logDurations
+                    ? `[Took ${
+                        formatDuration( timestamps.endedAt - timestamps.startedAt, timeGranularity)
+                    }${timeGranularity}]`
+                    : '';
+                console.log(`${ts}${duration} ${queryType}`, { query, params, result: opts?.logResults ? result : undefined })
             } : undefined,
             onQueryError: logEnd ? (queryType, query, params, error, timestamps) => {
-                const ts = opts?.logTimestamps ? `[${timestamps.endedAt}]` : '';
-                const duration = opts?.logDurations ? `[Took ${(timestamps.endedAt - timestamps.startedAt)}ns]` : '';
+                const ts = opts?.logTimestamps
+                    ? `[${formatDuration(timestamps.endedAt, timeGranularity)}]`
+                    : '';
+                const duration = opts?.logDurations
+                    ? `[Took ${
+                        formatDuration( timestamps.endedAt - timestamps.startedAt, timeGranularity)
+                    }${timeGranularity}]`
+                    : '';
                 console.log(`${ts}${duration} ${queryType}`, { query, params, error })
             } : undefined
         }, queryRunner)
+    }
+}
+
+const formatDuration = (durationNS: bigint, granularity: TimeGranularity) => {
+    switch (granularity) {
+        case 'ns': return durationNS;
+        case 'us': return durationNS/BigInt("1000");
+        case 'ms': return durationNS/BigInt("1000000");
     }
 }

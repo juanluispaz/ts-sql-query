a77a9d84f60b0d1b893f349a342f8dfad56d5ae4
juanluispaz <juanluispaz@gmail.com>
2022-08-07 05:19:52 +0200

    Add tests in pipeline

---
+++ scripts/run-no-docker-examples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x #echo on
+
+ts-node ./src/examples/documentation/Sqlite-compatibility.ts || exit 1
+ts-node ./src/examples/documentation/Sqlite-modern.ts || exit 1
+ts-node ./src/examples/documentation/PostgreSql.ts || exit 1
+ts-node ./src/examples/documentation/MySql.ts || exit 1
+ts-node ./src/examples/documentation/MySql-compatibility.ts || exit 1
+ts-node ./src/examples/documentation/MariaDB.ts || exit 1
+ts-node ./src/examples/documentation/SqlServer.ts || exit 1
+ts-node ./src/examples/documentation/Oracle.ts || exit 1
+
+ts-node ./src/examples/SqliteExample.ts || exit 1
+ts-node ./src/examples/Sqlite3Example.ts || exit 1
+ts-node ./src/examples/BetterSqlite3Example.ts || exit 1
+ts-node ./src/examples/BetterSqlite3SynchronousExample.ts || exit 1
+ts-node ./src/examples/AnyDBSqlite3Example.ts || exit 1
+ts-node ./src/examples/LoopBackSqlite3Example.ts || exit 1
+#ts-node ./src/examples/PrismaSqliteExample.ts || exit 1
+
+echo 'All no docker examples ok'

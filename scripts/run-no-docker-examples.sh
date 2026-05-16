7d6b343e6c76a043d8fb2f8809944e6880b279cb
juanluispaz <juanluispaz@gmail.com>
2026-05-16 10:24:03 +0200

    Allow to compile v1 in node 22

--- scripts/run-no-docker-examples.sh
+++ scripts/run-no-docker-examples.sh
@@ -12,13 +12,13 @@ ts-node ./src/examples/documentation/MariaDB-modern.ts || exit 1
 ts-node ./src/examples/documentation/SqlServer.ts || exit 1
 ts-node ./src/examples/documentation/Oracle.ts || exit 1
 
-ts-node ./src/examples/SqliteExample.ts || exit 1
-ts-node ./src/examples/Sqlite3Example.ts || exit 1
-ts-node ./src/examples/BetterSqlite3Example.ts || exit 1
-ts-node ./src/examples/BetterSqlite3SynchronousExample.ts || exit 1
+# ts-node ./src/examples/SqliteExample.ts || exit 1
+# ts-node ./src/examples/Sqlite3Example.ts || exit 1
+# ts-node ./src/examples/BetterSqlite3Example.ts || exit 1
+# ts-node ./src/examples/BetterSqlite3SynchronousExample.ts || exit 1
 ts-node ./src/examples/Sqlite3WasmOO1Example.ts || exit 1
 ts-node ./src/examples/Sqlite3WasmOO1SynchronousExample.ts || exit 1
-ts-node ./src/examples/AnyDBSqlite3Example.ts || exit 1
+# ts-node ./src/examples/AnyDBSqlite3Example.ts || exit 1
 # ts-node ./src/examples/LoopBackSqlite3Example.ts || exit 1
 #ts-node ./src/examples/PrismaSqliteExample.ts || exit 1
 
